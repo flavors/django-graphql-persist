d5f6d1a6cfec9c895c387cf83e87f669b9ba222e
mongkok <domake.io@gmail.com>
2018-04-05 19:51:07 +0700

    Improved strip relay tags

--- graphql_persist/renderers/relay.py
+++ graphql_persist/renderers/relay.py
@@ -3,28 +3,24 @@ from .base import BaseRenderer
 __all__ = ['StripRelayTagsRenderer']
 
 
+def strip_edges(data, extra=None):
+    results = strip_relay_tags(data)
+    if not extra:
+        return results
+    extra['results'] = results
+    return extra
+
+
 def strip_relay_tags(data):
     if isinstance(data, dict):
         edges = data.pop('edges', None)
-
         if edges is not None:
-            results = strip_relay_tags(edges)
-
-            if not data:
-                return results
-
-            data['results'] = results
-
-        else:
-            node = data.pop('node', None)
-
-            if node is not None:
-                return strip_relay_tags(node)
-
+            return strip_edges(edges, data)
+        elif 'node' in data:
+            return strip_relay_tags(data['node'])
         ret = {}
         for k, v in data.items():
             ret[k] = strip_relay_tags(v)
-
     elif isinstance(data, list):
         ret = []
         for v in data:
