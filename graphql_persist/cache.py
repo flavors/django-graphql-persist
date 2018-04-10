2f173d34befdf1f67c0b669c24f4ac44b673c714
mongkok <domake.io@gmail.com>
2018-04-10 22:27:57 +0700

    Removed new line

--- graphql_persist/cache.py
+++ graphql_persist/cache.py
@@ -4,7 +4,6 @@ from django.core.cache.backends.base import InvalidCacheBackendError
 
 from .settings import persist_settings
 
-
 try:
     cache = caches[persist_settings.CACHE_NAME]
 except (InvalidCacheBackendError, ValueError):
