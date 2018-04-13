8d5e2b18727565fc93f3ea757b7649bc5d4ba7cb
mongkok <domake.io@gmail.com>
2018-04-14 01:52:04 +0700

    Fixed loaders Cached key

--- graphql_persist/loaders/cached.py
+++ graphql_persist/loaders/cached.py
@@ -8,11 +8,11 @@ class CachedEngine(Engine):
         self.get_document_cache = {}
 
     def get_document(self, query_key):
-        document = self.get_document_cache.get(query_key)
+        document = self.get_document_cache.get(str(query_key))
 
         if document is None:
             document = super().get_document(query_key)
-            self.get_document_cache[query_key] = document
+            self.get_document_cache[str(query_key)] = document
         return document
 
     def reset(self):
