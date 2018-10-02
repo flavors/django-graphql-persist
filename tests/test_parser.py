faf6c42e0e884cd7865381d582872a7e8f86d7a9
mongkok <domake.io@gmail.com>
2018-10-03 01:21:17 +0700

    Added parser tests

---
+++ tests/test_parser.py
@@ -0,0 +1,44 @@
+import json
+
+from django.test import RequestFactory, testcases
+
+from graphql_persist import parser
+
+
+class ParserTests(testcases.TestCase):
+
+    def setUp(self):
+        self.factory = RequestFactory()
+
+    def test_application_json(self):
+        request = self.factory.post(
+            '/',
+            data=json.dumps({'test': True}),
+            content_type='application/json')
+
+        result = parser.parse_body(request)
+        self.assertTrue(result['test'])
+
+    def test_json_decode_error(self):
+        request = self.factory.post(
+            '/',
+            data='error',
+            content_type='application/json')
+
+        result = parser.parse_body(request)
+        self.assertIsNone(result)
+
+    def test_x_www_form_urlencoded(self):
+        request = self.factory.post('/', data={'test': True})
+        result = parser.parse_body(request)
+
+        self.assertTrue(eval(result['test']))
+
+    def test_unknown_content_type(self):
+        request = self.factory.post(
+            '/',
+            data={'test': True},
+            content_type='unknown')
+
+        result = parser.parse_body(request)
+        self.assertIsNone(result)
